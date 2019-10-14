e0c8d15f3335c8928b39ca89f75265df2d0238dd
Max Onishi <max@bluechilli.com>
2019-10-14 12:59:41 +1100

    reverse android change

--- android/src/main/java/com/bluechilli/flutteruploader/UploadWorker.java
+++ android/src/main/java/com/bluechilli/flutteruploader/UploadWorker.java
@@ -129,10 +129,8 @@ public class UploadWorker extends Worker implements CountProgressListener {
 
         if (file.exists() && file.isFile()) {
           fileExistsCount++;
-          String mimeType = GetMimeType(item.getPath());
-          MediaType contentType = MediaType.parse(mimeType);
           RequestBody fileBody =
-              RequestBody.create(file, contentType);
+                  RequestBody.create(MediaType.parse(GetMimeType(item.getPath())), file);
           formRequestBuilder.addFormDataPart(item.getFieldname(), item.getFilename(), fileBody);
         } else {
           Log.d(TAG, "File does not exists -> file:" + item.getPath());
