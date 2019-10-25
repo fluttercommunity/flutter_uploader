de9e9059228451b55e21a233e5c8ef67aa3364ac
Sebastian Roth <sebastian.roth@gmail.com>
2019-10-25 11:56:21 +0100

    define clang module

--- ios/Classes/FlutterUploaderPlugin.m
+++ ios/Classes/FlutterUploaderPlugin.m
@@ -1,7 +1,10 @@
 #import "FlutterUploaderPlugin.h"
+
+#if __has_include(<flutter_uploader/flutter_uploader-Swift.h>)
 #import <flutter_uploader/flutter_uploader-Swift.h>
-
-
+#else
+#import "flutter_uploader-Swift.h"
+#endif
 
 @implementation FlutterUploaderPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
