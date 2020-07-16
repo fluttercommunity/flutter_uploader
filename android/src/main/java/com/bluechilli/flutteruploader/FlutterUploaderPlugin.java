6fadff64b020927c99f2ac2ea13873e3a73b1932
Sebastian Roth <sebastian@nextbase.co.uk>
2020-07-16 11:10:08 +0100

    Some CI fixes

--- android/src/main/java/com/bluechilli/flutteruploader/FlutterUploaderPlugin.java
+++ android/src/main/java/com/bluechilli/flutteruploader/FlutterUploaderPlugin.java
@@ -1,7 +1,6 @@
 package com.bluechilli.flutteruploader;
 
 import android.content.Context;
-import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.bluechilli.flutteruploader.plugin.StatusListener;
