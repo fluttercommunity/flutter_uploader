dab84c6208e807a18c1df0fec43de9dec4a2df5a
Sebastian Roth <sebastian@nextbase.co.uk>
2020-07-22 11:57:09 +0100

    Simplify cloud functions backend, remove Express JS

--- example/backend/functions/index.js
+++ example/backend/functions/index.js
@@ -1,15 +1,12 @@
-const express = require("express");
-const compression = require("compression");
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
 const functions = require("firebase-functions");
-const cors = require("cors");
 const Busboy = require("busboy");
 const md5File = require('md5-file');
 const crypto = require("crypto");
 
-const formUploadHandler = (req, res) => {
+exports.upload = functions.https.onRequest(async (req, res) => {
   const uploads = [];
   const methods = ["POST", "PUT", "PATCH"];
   if (!methods.includes(req.method, 0)) {
@@ -24,8 +21,12 @@ const formUploadHandler = (req, res) => {
 
   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
     console.log("File [" + fieldname + "]: filename: " + filename);
+
+    let length = 0;
+
     file.on("data", data => {
       console.log("File [" + fieldname + "] got " + data.length + " bytes");
+      length = data.length ;
     });
     file.on("end", () => {
       console.log("File [" + fieldname + "] Finished");
@@ -38,6 +39,7 @@ const formUploadHandler = (req, res) => {
       filename: filename,
       mimetype: mimetype,
       encoding: encoding,
+      length: length,
     });
     console.log(`Saving '${fieldname}' to ${filepath}`);
     file.pipe(fs.createWriteStream(filepath));
@@ -63,28 +65,22 @@ const formUploadHandler = (req, res) => {
       response.random = crypto.randomBytes(10240).toString('hex');
     }
 
-    res
-      .status(statusCode)
-      .json({
-        message: "Successfully uploaded",
-        request: response
-      })
-      .end();
+    res.status(statusCode).json({
+      message: "Successfully uploaded",
+      request: response
+    }).end();
   });
   busboy.on("error", error => {
-    res
-      .status(500)
-      .json({
-        errorMessage: error.toString(),
-        error: error
-      })
-      .end();
+    res.status(500).json({
+      errorMessage: error.toString(),
+      error: error
+    }).end();
   });
   busboy.end(req.rawBody);
   return req.pipe(busboy);
-};
+});
 
-const binaryUploadHandler = async (req, res) => {
+exports.uploadBinary = functions.https.onRequest(async (req, res) => {
   const methods = ["POST", "PUT", "PATCH"];
   if (!methods.includes(req.method, 0)) {
     return res.status(405).json({
@@ -93,13 +89,19 @@ const binaryUploadHandler = async (req, res) => {
   }
 
   const simulate = req.query.simulate !== undefined ? req.query.simulate : 'ok200';
-
   const filename = [...Array(10)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
   const filepath = path.join(os.tmpdir(), filename);
 
-  console.log("File [" + filename + "]: filepath: " + filepath);
+  console.log(`File [${filename}]: filepath: ${filepath}, ${JSON.stringify(req.headers)}`);
 
-  await writeToFile(filepath, req.rawBody);
+  try {
+    await writeToFile(filepath, req.rawBody);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      message: "Invalid request",
+    }).end();
+  }
 
   const md5hash = md5File.sync(filepath);
   const stats = fs.statSync(filepath);
@@ -113,8 +115,12 @@ const binaryUploadHandler = async (req, res) => {
     message: "Successfully uploaded",
     length: fileSizeInBytes,
     md5: md5hash,
+    headers: req.headers,
+    method: req.method,
   }).end();
-};
+});
+
+
 
 function statusCodeForSimulation(simulation) {
   switch (simulation) {
@@ -144,18 +150,3 @@ function writeToFile(filePath, rawBody) {
     file.on("error", reject);
   });
 }
-
-const app = express();
-app.use(cors({ origin: true }));
-app.use(compression());
-
-app.post("/", formUploadHandler);
-app.put("/", formUploadHandler);
-app.patch("/", formUploadHandler);
-
-app.post("/binary", binaryUploadHandler,);
-app.put("/binary", binaryUploadHandler);
-app.patch("/binary", binaryUploadHandler);
-
-exports.upload = functions.https.onRequest(app);
-functions.https.onRequest
