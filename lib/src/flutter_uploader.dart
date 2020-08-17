8edc2080efb485f8b8637c2db5bc533e1511613e
Pascal Stech <pascal@neohelden.com>
2020-08-17 13:37:52 +0200

    Keep progress and result stream instance

--- lib/src/flutter_uploader.dart
+++ lib/src/flutter_uploader.dart
@@ -5,6 +5,9 @@ class FlutterUploader {
   final EventChannel _progressChannel;
   final EventChannel _resultChannel;
 
+  Stream<UploadTaskProgress> _progressStream;
+  Stream<UploadTaskResponse> _resultStream;
+
   static FlutterUploader _instance;
 
   factory FlutterUploader() {
@@ -40,56 +43,67 @@ class FlutterUploader {
   /// stream to listen on upload progress
   ///
   Stream<UploadTaskProgress> get progress {
-    return _progressChannel
-        .receiveBroadcastStream()
-        .map<Map<String, dynamic>>((event) => Map<String, dynamic>.from(event))
-        .transform(StreamTransformer<Map<String, dynamic>,
-            UploadTaskProgress>.fromHandlers(
-      handleData:
-          (Map<String, dynamic> map, EventSink<UploadTaskProgress> sink) {
-        String id = map['taskId'];
-        int status = map['status'];
-        int uploadProgress = map['progress'];
-        final t = UploadTaskProgress(
-            id, uploadProgress, UploadTaskStatus.from(status));
+    if (_progressStream == null) {
+      _progressStream = _progressChannel
+          .receiveBroadcastStream()
+          .map<Map<String, dynamic>>(
+              (event) => Map<String, dynamic>.from(event))
+          .transform(StreamTransformer<Map<String, dynamic>,
+              UploadTaskProgress>.fromHandlers(
+        handleData:
+            (Map<String, dynamic> map, EventSink<UploadTaskProgress> sink) {
+          String id = map['taskId'];
+          int status = map['status'];
+          int uploadProgress = map['progress'];
+          final t = UploadTaskProgress(
+              id, uploadProgress, UploadTaskStatus.from(status));
 
-        sink.add(t);
-      },
-    ));
+          sink.add(t);
+        },
+      ));
+    }
+
+    return _progressStream;
   }
 
   ///
   /// stream to listen on upload result
   ///
   Stream<UploadTaskResponse> get result {
-    return _resultChannel
-        .receiveBroadcastStream()
-        .map<Map<String, dynamic>>((event) => Map<String, dynamic>.from(event))
-        .transform(
-      StreamTransformer<Map<String, dynamic>, UploadTaskResponse>.fromHandlers(
-        handleData:
-            (Map<String, dynamic> value, EventSink<UploadTaskResponse> sink) {
-          String id = value['taskId'];
-          String message = value['message'];
-          // String code = value['code'];
-          int status = value["status"];
-          int statusCode = value["statusCode"];
-          Map<String, dynamic> headers = value['headers'] != null
-              ? Map<String, dynamic>.from(value['headers'])
-              : {};
+    if (_resultStream == null) {
+      _resultStream = _resultChannel
+          .receiveBroadcastStream()
+          .map<Map<String, dynamic>>(
+              (event) => Map<String, dynamic>.from(event))
+          .transform(
+        StreamTransformer<Map<String, dynamic>,
+            UploadTaskResponse>.fromHandlers(
+          handleData:
+              (Map<String, dynamic> value, EventSink<UploadTaskResponse> sink) {
+            String id = value['taskId'];
+            String message = value['message'];
+            // String code = value['code'];
+            int status = value["status"];
+            int statusCode = value["statusCode"];
+            Map<String, dynamic> headers = value['headers'] != null
+                ? Map<String, dynamic>.from(value['headers'])
+                : {};
 
-          final r = UploadTaskResponse(
-            taskId: id,
-            status: UploadTaskStatus.from(status),
-            statusCode: statusCode,
-            headers: headers,
-            response: message,
-          );
+            final r = UploadTaskResponse(
+              taskId: id,
+              status: UploadTaskStatus.from(status),
+              statusCode: statusCode,
+              headers: headers,
+              response: message,
+            );
 
-          sink.add(r);
-        },
-      ),
-    );
+            sink.add(r);
+          },
+        ),
+      );
+    }
+
+    return _resultStream;
   }
 
   void dispose() {
